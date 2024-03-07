6fad77cbcc3fd66437c326e5014ff04bc2a7aa24
Umbrella167 <1670187757@qq.com>
2024-03-07 23:11:59 +0800

    test3

--- Project/USER/inc/PID.h
+++ Project/USER/inc/PID.h
@@ -3,27 +3,6 @@
 
 #include "headfile.h"
 
-typedef struct 
-{
-    float SumError;  //误差累积
 
-    float Kp;      //比例系数
-    float Ki;      //积分系数
-    float Kd;      //微分系数
-
-    float LastError;  //上一次误差
-    float LLastError; //上上次误差
-}PID;
-
-//结构体变量申明
-extern PID TurnPID;
-extern PID SpeedPID;
-extern PID Turn_NeiPID;
-
-//函数声明
-void IncPIDInit(PID *sptr);
-int16 LocP_DCalc(PID*sptr,int16 Setpoint,int16 Turepoint);
-int16 IncPIDCalc(PID *sptr,int16 Setpoint,int16 Turepoint);//增量式PID控制
-int16 PlacePID_Control(PID *sptr, int16 Setpoint,int16 Turepiont);//动态位置式PID控制
 
 #endif
